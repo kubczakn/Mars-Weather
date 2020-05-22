059654ee9e9b218d53d46c670f4b4b88fe791b4a
kubczakn <kubczakn@umich.edu>
2020-05-22 15:50:24 -0400

    basic get and post functionality

--- index.js
+++ index.js
@@ -5,47 +5,36 @@ const axios = require('axios').default;
 
 const apiKey = 'MbmiM3rQcY20x4HTBDEXzg6ecDMsjOULRMf8IKJS';
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=MbmiM3rQcY20x4HTBDEXzg6ecDMsjOULRMf8IKJS';
-/*
-const params = {
-    access_key: apiKey,
-    query: '259'
-}
-*/
-// Enables parsing of JSON objects in body of request
+const url = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`;
+
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-axios.get(url)
-    .then(response => {
-        const apiResponse = response.data;
-        console.log(apiResponse);
-    }).catch(error => {
-        console.log(error);
-    });
+const solData = [];
 
-app.delete('/', (req, res) => {
+async function makePostRequest() {
+    let res = await axios.post(url);
+    let sol = res.data.sol_keys[0];
+    let temperature = res.data[sol].AT.av;
+    console.log(`Status code: ${res.status}`);
+    console.log(`Temperature: ${temperature}`);
+    let data = {
+        id: solData.length + 1,
+        temperature: temperature
+    }
+    solData.push(data);
+}
 
-});
+app.get('/', (req, res) => {
+    res.send(solData);
+})
 
-app.put('/', (req,res) => {
-
-});
-
-
-app.post('/', (req, res) => {  
-    
-});
+app.post('/', (req, res) => {
+    makePostRequest();
+    res.send(solData);
+})
 
 // POST Environment Varaible
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-function validate(item) {
-    const schema = Joi.object({
-
-    }).required();
-
-    return schema.validate(item);
-}
